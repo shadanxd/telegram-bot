51bb3586f1234f6d1c601eacf8fddd53d6055201
shadanxd <shadankalam@gmail.com>
2023-09-27 23:18:31 +0530

    added bot token in config.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import os
 from telegram import Update, InputFile
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
 from downloader import Downloader
+import config
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -47,8 +48,7 @@ async def upload(update: Update, context: ContextTypes.DEFAULT_TYPE, path):
         logging.error(f"Error uploading or deleting file: {e}")
     
 if __name__ == '__main__':
-    application = ApplicationBuilder().token(
-        '6371821489:AAGObhHZiXqvEgV-IQGtcBG_IdqE5aWMkBM').build()
+    application = ApplicationBuilder().token(config.bot_token).build()
     
     start_handler = CommandHandler('start', start)
     link_handler = CommandHandler('link', link)
